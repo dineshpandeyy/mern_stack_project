61d4c7032b201aa21d5e9a20944c42d18dac460f
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-11-16 21:17:36 -0600

    set up the project directory

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Register from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
