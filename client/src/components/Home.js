5bf6ae493fb3de02faf50ac8a4b36130fb9d09ab
Dinesh Pandey <dineshpandey3536@gmail.com>
2022-11-18 22:35:46 -0600

    worked with react-router-dom

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import {NavLink} from "react-router-dom";
 
 
 const Home = () => {
+
   return (
       <>
         <div className="container mt-2">
@@ -12,20 +13,46 @@ const Home = () => {
                 Football blog project
             </h1>
             <div className="text-end">
-                <Button variant="success">Add Blog</Button>{' '}
+                <Button variant="success"><NavLink to="/register" className="text-decoration-none text-black">Add Blog</NavLink></Button>{' '}
             </div>
 
             <div className="row d-flex justify-content-between align-items-center mt-5">
-                <Card className="mb-3" style={{ width: '22rem',height:"18rem"}}>
-                <Card.Img style={{ width: '100px',textAlign:"center",margin:"auto"}} variant="top" src="holder.js/100px180" />
-                <Card.Body>
+                <Card className="mb-3" style={{ width: '22rem',height:"30rem"}}>
+                <Card.Img style={{ width: '300px',textAlign:"center",margin:"auto"}} variant="top" src="https://cdn.vox-cdn.com/thumbor/Tt0f2HXoG2uE13gdlCc7_oUj8rw=/0x0:3893x2950/1200x800/filters:focal(1507x247:2129x869)/cdn.vox-cdn.com/uploads/chorus_image/image/70812194/1394004337.0.jpg" />
+                <Card.Body className="text-center">
                     <Card.Title>Real Madrid vs Man City</Card.Title>
                     <Card.Text>
                         The night of the Real resurrection! The pure white shirts have shimmered 
                         spectacularly once again. And Real Madrid are Paris-bound. Real Madrid will
                          play the big game again. The love affair goes on.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Button variant="danger" className="col-lg-6 text-center">Delete</Button>
+                </Card.Body>
+                </Card>
+
+                <Card className="mb-3" style={{ width: '22rem',height:"30rem"}}>
+                <Card.Img style={{ width: '300px',textAlign:"center",margin:"auto"}} variant="top" src="https://cdn.vox-cdn.com/thumbor/Tt0f2HXoG2uE13gdlCc7_oUj8rw=/0x0:3893x2950/1200x800/filters:focal(1507x247:2129x869)/cdn.vox-cdn.com/uploads/chorus_image/image/70812194/1394004337.0.jpg" />
+                <Card.Body className="text-center">
+                    <Card.Title>Real Madrid vs Man City</Card.Title>
+                    <Card.Text>
+                        The night of the Real resurrection! The pure white shirts have shimmered 
+                        spectacularly once again. And Real Madrid are Paris-bound. Real Madrid will
+                         play the big game again. The love affair goes on.
+                    </Card.Text>
+                    <Button variant="danger" className="col-lg-6 text-center">Delete</Button>
+                </Card.Body>
+                </Card>
+
+                <Card className="mb-3" style={{ width: '22rem',height:"30rem"}}>
+                <Card.Img style={{ width: '300px',textAlign:"center",margin:"auto"}} variant="top" src="https://cdn.vox-cdn.com/thumbor/Tt0f2HXoG2uE13gdlCc7_oUj8rw=/0x0:3893x2950/1200x800/filters:focal(1507x247:2129x869)/cdn.vox-cdn.com/uploads/chorus_image/image/70812194/1394004337.0.jpg" />
+                <Card.Body className="text-center">
+                    <Card.Title>Real Madrid vs Man City</Card.Title>
+                    <Card.Text>
+                        The night of the Real resurrection! The pure white shirts have shimmered 
+                        spectacularly once again. And Real Madrid are Paris-bound. Real Madrid will
+                         play the big game again. The love affair goes on.
+                    </Card.Text>
+                    <Button variant="danger" className="col-lg-6 text-center">Delete</Button>
                 </Card.Body>
                 </Card>
             </div>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
 
 const Register = () => {
+
+  const [fname,setFName] = useState("");
+  const [file,setFile] = useState("");
+
+  const setData = (e) => {
+    setFName(e.target.value)
+  }
+
+  const setImgFile = (e) => {
+    setFile(e.target.files[0])
+  }
+
   return (
-    <div>Register
+    <>
+    <div className="containe mt-3">
+      <h1>Upload your img here</h1>
+      <Form className="mt-3">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>User Name</Form.Label>
+        <Form.Control type="text" placeholder="" name='fname' onChange={setData}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Select your image</Form.Label>
+        <Form.Control type="file" placeholder="" name="photo" onChange={setImgFile}/>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     </div>
+    </>
   )
 }
 
